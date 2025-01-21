5afc900f488d7ccecb0164bb5a9330e7c562ef74
robin <rirobink@gmail.com>
2025-01-21 22:23:51 +0600

    laod dataset csv from file

--- task_manager.py
+++ task_manager.py
@@ -3,3 +3,15 @@ import json
 import csv
 from datetime import datetime, timedelta
 
+# dataset
+file_name = "task_manager_dataset.csv"
+
+
+def load_tasks_from_file(file_name):
+    if not os.path.exists(file_name):
+        return []
+    with open(file_name, 'r', newline='') as file:
+        reader = csv.DictReader(file)
+        print('dataset loaded Total task is :', len(list(reader)))
+        return list(reader)
+load_tasks_from_file(file_name)
