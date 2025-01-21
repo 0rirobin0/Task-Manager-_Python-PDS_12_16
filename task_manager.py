597d9bef55ae43974666b2941182739fbf34ad4b
robin <rirobink@gmail.com>
2025-01-21 22:40:06 +0600

    added new task , display all tasks

--- task_manager.py
+++ task_manager.py
@@ -12,6 +12,42 @@ def load_tasks_from_file(file_name):
         return []
     with open(file_name, 'r', newline='') as file:
         reader = csv.DictReader(file)
-        print('dataset loaded Total task is :', len(list(reader)))
         return list(reader)
-load_tasks_from_file(file_name)
+    
+
+# save task to file
+def save_tasks_to_file(tasks, file_name):
+    with open(file_name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Title", "Description", "Priority", "Deadline", "Status"])
+        for task in tasks:
+            writer.writerow([task["Title"], task["Description"], task["Priority"], task["Deadline"], task["Status"]])
+
+# add new task
+def add_task(tasks):
+    title = input("Enter task title: ")
+    description = input("Enter task description: ")
+    priority = input("Enter priority (High, Medium, Low): ").capitalize()
+    deadline = input("Enter deadline (YYYY-MM-DD) or leave blank: ")
+    if deadline:
+        try:
+            deadline = datetime.strptime(deadline, "%Y-%m-%d").strftime("%Y-%m-%d")
+        except ValueError:
+            print("Invalid date format. Skipping deadline.")
+            deadline = None
+    else:
+        deadline = None
+    tasks.append({"Title": title, "Description": description, "Priority": priority, "Deadline": deadline, "Status": "Pending"})
+    save_tasks_to_file(tasks, file_name)
+
+# Display all Task
+def display_Tasks(tasks):
+    for task in tasks:
+                print(f"{task['Title']},{task['Description'][:10]}...,{task['Priority']},{task['Status']}, (Deadline: {task['Deadline']})")
+         
+
+
+
+tasks = load_tasks_from_file(file_name)
+# add_task(tasks)
+display_Tasks(tasks)
